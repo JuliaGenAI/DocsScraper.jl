ce2f12c9391fa1db4015b92f2c2dee3e295a1a1f
Shreyas Agrawal <48771895+splendidbug@users.noreply.github.com>
2024-06-19 19:15:33 -0700

    added tests and code improvements

--- src/crawl.jl
+++ src/crawl.jl
@@ -1,9 +1,16 @@
-include("parser.jl")
-## TODO: Make multiple dispatch for the following function
-function parse_robots_txt!(robots_txt::String, url_queue::Vector{<:AbstractString})
-    ## TODO: Make a cache of rules for a quick lookup
+## TODO: Make multiple dispatch for the following function to remove if-else
+"""
+    parse_robots_txt!(robots_txt::String)
+
+Parses the robots.txt string and returns rules along with the URLs on Sitemap
+
+# Arguments
+- `robots_txt`: robots.txt as a string
+"""
+function parse_robots_txt!(robots_txt::String)
     rules = Dict{String,Dict{String,Vector{String}}}()
     current_user_agent = ""
+    sitemap_urls = Vector{AbstractString}()
 
     for line in split(robots_txt, '\n')
         line = strip(line)
@@ -24,20 +31,28 @@ function parse_robots_txt!(robots_txt::String, url_queue::Vector{<:AbstractStrin
             end
         elseif startswith(line, "Sitemap:")
             url = strip(split(line, ":")[2])
-            push!(url_queue, url)
+            push!(sitemap_urls, url)
         end
 
     end
-    return rules
+    return rules, sitemap_urls
 end
 
 
-function check_robots_txt(user_agent::AbstractString,
-    url::AbstractString,
-    url_queue::Vector{<:AbstractString})
+"""
+    check_robots_txt(user_agent::AbstractString,
+        url::AbstractString)
 
-    URI = URIs.URI(url)
-    path = URI.path
+Checks the robots.txt of a URL and returns a boolean representing if `user_agent` is allowed to crawl the input url
+
+# Arguments
+- `user_agent`: user agent attempting to crawl the webpage
+- `url`: input URL string
+"""
+function check_robots_txt(user_agent::AbstractString,
+    url::AbstractString)
+
+    ## TODO: Make a cache of rules for a quick lookup
     # if (haskey(restricted_urls, url))
     #     if (in(path, restricted_urls[url]))
     #         println("Not allowed to crawl $url")
@@ -47,11 +62,15 @@ function check_robots_txt(user_agent::AbstractString,
     #     end
     # end
 
+    URI = URIs.URI(url)
+    path = URI.path
+
     robots_URL = string(URI.scheme, "://", URI.host, "/robots.txt")
+    sitemap_urls = Vector{AbstractString}()
     try
         response = HTTP.get(robots_URL)
         robots_txt = String(response.body)
-        rules = parse_robots_txt!(robots_txt, url_queue)
+        rules, sitemap_urls = parse_robots_txt!(robots_txt)
         user_agents = [user_agent, "*"]
         for ua in user_agents
             if haskey(rules, ua)
@@ -60,25 +79,24 @@ function check_robots_txt(user_agent::AbstractString,
 
                 for allow_rule in allow_rules
                     if startswith(path, allow_rule)
-                        return true
+                        return true, sitemap_urls
                     end
                 end
 
                 for disallow_rule in disallow_rules
                     if startswith(path, disallow_rule)
                         @warn "Not allowed to crawl $url"
-                        return false
+                        return false, sitemap_urls
                     end
                 end
             end
         end
-        return true
+        return true, sitemap_urls
     catch
         @info "robots.txt unavailable for $url"
-        return true
+        return true, sitemap_urls
     end
 end
-
 
 """
     get_base_url(url::AbstractString)
@@ -96,46 +114,33 @@ function get_base_url(url::AbstractString)
     return base_url
 end
 
-## TODO: Function to Check for version number
 
-# function get_domain_name(hostname::String)
-#     parts = split(hostname, '.', limit=2)
-#     if length(parts) == 2
-#         domain = parts[2]
-#         # Remove the last part of the domain after the last dot (the TLD)
-#         domain_parts = split(domain, '.')
-#         if length(domain_parts) > 1
-#             return join(domain_parts[1:end-1], ".")  # Join all parts except the last one
-#         else
-#             return domain  # If there is no TLD, return the domain itself
-#         end
-#     else
-#         return hostname  # If there is no dot, return the hostname itself
-#     end
-# end
+"""
+    process_hostname(url::AbstractString)
 
+Returns the hostname of an input URL
 
-function process_hostname(url)
+# Arguments
+- `url`: URL string
+"""
+function process_hostname(url::AbstractString)
     URI = URIs.URI(url)
     hostname = String(URI.host)
     return hostname
 end
 
-function report_artifact(fn_output)
-    println("ARTIFACT: $(basename(fn_output))")
-    println("sha256: ", bytes2hex(open(sha256, fn_output)))
-    println("git-tree-sha1: ", Tar.tree_hash(IOBuffer(inflate_gzip(fn_output))))
-end
 
-# Function to group URLs by hostname
-## TODO: Find out if dict is passed by reference. if it is, remove the return statement
-function process_hostname(url::AbstractString, hostname_dict)
-    # println("----------------------------")
-    # println("hostnames:")
-    # for url in urls
+"""
+    process_hostname(url::AbstractString, hostname_dict::Dict{AbstractString,Vector{AbstractString}})
+
+Adds the `url` to it's hostname in `hostname_dict`
+
+# Arguments
+- `url`: URL string
+- `hostname_dict`: Dict with key being hostname and value being a vector of URLs
+"""
+function process_hostname!(url::AbstractString, hostname_dict::Dict{AbstractString,Vector{AbstractString}})
     hostname = process_hostname(url)
-    # println(URI.host)
-    # println(hostname)
 
     # Add the URL to the dictionary under its hostname
     if haskey(hostname_dict, hostname)
@@ -143,103 +148,23 @@ function process_hostname(url::AbstractString, hostname_dict)
     else
         hostname_dict[hostname] = [url]
     end
-    # end
-    return hostname_dict
-end
-
-
-function make_embeddings()
-    embedder = RT.BatchEmbedder()
-    dir_path = joinpath("RAGKit", "knowledge_packs")
-    entries = readdir(dir_path)
-
-    # Initialize a dictionary to group files by hostname and chunk size
-    hostname_files = Dict{String,Dict{Int,Dict{String,String}}}()
-
-    # Regular expressions to match the file patterns
-    chunks_pattern = r"^(.*)-chunks-(\d+)\.jls$"
-    sources_pattern = r"^(.*)-sources-(\d+)\.jls$"
-
-    # Group files by hostname and chunk size
-    for file in entries
-        match_chunks = match(chunks_pattern, file)
-        match_sources = match(sources_pattern, file)
-
-        if match_chunks !== nothing
-            hostname = match_chunks.captures[1]
-            chunk_size = parse(Int, match_chunks.captures[2])
-            if !haskey(hostname_files, hostname)
-                hostname_files[hostname] = Dict{Int,Dict{String,String}}()
-            end
-            if !haskey(hostname_files[hostname], chunk_size)
-                hostname_files[hostname][chunk_size] = Dict{String,String}()
-            end
-            hostname_files[hostname][chunk_size]["chunks"] = joinpath(dir_path, file)
-        elseif match_sources !== nothing
-            hostname = match_sources.captures[1]
-            chunk_size = parse(Int, match_sources.captures[2])
-            if !haskey(hostname_files, hostname)
-                hostname_files[hostname] = Dict{Int,Dict{String,String}}()
-            end
-            if !haskey(hostname_files[hostname], chunk_size)
-                hostname_files[hostname][chunk_size] = Dict{String,String}()
-            end
-            hostname_files[hostname][chunk_size]["sources"] = joinpath(dir_path, file)
-        end
-    end
-
-
-    # Process each pair of files
-    for (hostname, chunk_files) in hostname_files
-        for (chunk_size, files) in chunk_files
-            if haskey(files, "chunks") && haskey(files, "sources")
-                chunks_file = files["chunks"]
-                sources_file = files["sources"]
-                chunks = deserialize(chunks_file)
-                sources = deserialize(sources_file)
-                cost_tracker = Threads.Atomic{Float64}(0.0)
-                full_embeddings = RT.get_embeddings(embedder, chunks; model="text-embedding-3-large", verbose=false, cost_tracker, api_key=ENV["OPENAI_API_KEY"])
-
-                # Float32
-                fn_output = joinpath(dir_path, "packs", "$hostname-textembedding3large-0-Float32__v1.0.tar.gz")
-                fn_temp = joinpath(dir_path, "packs", "pack.hdf5")
-                h5open(fn_temp, "w") do file
-                    file["chunks"] = chunks
-                    file["sources"] = sources
-                    file["embeddings"] = full_embeddings
-                    file["type"] = "ChunkIndex"
-                    # file["metadata"] = "$hostname ecosystem docstrings, chunk size $chunk_size, downloaded on 20240330, contains: Makie.jl, AlgebraOfGraphics.jl, GeoMakie.jl, GraphMakie.jl, MakieThemes.jl, TopoPlots.jl, Tyler.jl"
-                end
-                run(`tar -cvzf $fn_output -C $(dirname(fn_temp)) $(basename(fn_temp))`)
-                report_artifact(fn_output)
-
-            else
-                @warn "Missing pair for hostname: $hostname, chunk size: $chunk_size"
-            end
-        end
-    end
-
 end
 
 
 """
-    makeRAG(input_urls::Vector{<:AbstractString})
+    crawl(input_urls::Vector{<:AbstractString})
 
-Extracts the base url.
+Crawls on the input URLs and returns a `hostname_url_dict` which is a dictionary with key being hostnames and the values being the URLs
 
 # Arguments
-- `input_urls`: vector containing URL strings to parse
+- `input_urls`: A vector of input URLs
 """
 function crawl(input_urls::Vector{<:AbstractString})
 
     url_queue = Vector{AbstractString}(input_urls)
     visited_url_set = Set{AbstractString}()
-    parsed_blocks = []
     hostname_url_dict = Dict{AbstractString,Vector{AbstractString}}()
-
-    # process_paths(input_urls[1])
-    # @info "done"
-    # return
+    sitemap_urls = Vector{AbstractString}()
 
     # TODO: Add parallel processing for URLs
     while !isempty(url_queue)
@@ -249,10 +174,12 @@ function crawl(input_urls::Vector{<:AbstractString})
 
         if !in(base_url, visited_url_set)
             push!(visited_url_set, base_url)
-            if check_robots_txt("*", base_url, url_queue)
+            crawlable, sitemap_urls = check_robots_txt("*", base_url)
+            append!(url_queue, sitemap_urls)
+            if crawlable
                 try
                     get_urls!(base_url, url_queue)
-                    hostname_url_dict = process_hostname(url, hostname_url_dict)
+                    process_hostname!(url, hostname_url_dict)
                 catch
                     @error "Bad URL: $base_url"
                 end
@@ -260,40 +187,6 @@ function crawl(input_urls::Vector{<:AbstractString})
         end
     end
 
-
-    # Define the folder path    
-    folder_path = joinpath("RAGKit", "knowledge_packs", "packs")
-
-    # Check if the folder exists
-    if !isdir(folder_path)
-        mkpath(folder_path)
-        @info "Folder created: $folder_path"
-    else
-        @info "Folder already exists: $folder_path"
-    end
-
-
-    output_chunks = Vector{SubString{String}}()
-    output_sources = Vector{String}()
-    SAVE_CHUNKS = true
-    CHUNK_SIZE = 512
-    for (hostname, urls) in hostname_url_dict
-        for url in urls
-            try
-                chunks, sources = process_paths(url)
-                append!(output_chunks, chunks)
-                append!(output_sources, sources)
-            catch
-                @error "error!! check url: $url"
-            end
-        end
-        if SAVE_CHUNKS
-            serialize(joinpath("RAGKit", "knowledge_packs", "$(hostname)-chunks-$(CHUNK_SIZE).jls"), output_chunks)
-            serialize(joinpath("RAGKit", "knowledge_packs", "$(hostname)-sources-$(CHUNK_SIZE).jls"), output_sources)
-        end
-
-    end
-
-    make_embeddings()
+    return hostname_url_dict
 
 end
