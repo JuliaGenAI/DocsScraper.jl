0782e01ba709f18bda1cca77cb83e8b73922630e
Shreyas Agrawal <48771895+splendidbug@users.noreply.github.com>
2024-08-11 03:19:20 -0700

    dependency changes

--- src/DocsScraper.jl
+++ src/DocsScraper.jl
@@ -2,8 +2,6 @@ module DocsScraper
 using HTTP, Gumbo, AbstractTrees, URIs
 using Gumbo: HTMLDocument, HTMLElement
 using EzXML
-using Pkg
-Pkg.develop(PackageSpec(path="C:\\Users\\shrey\\Desktop\\stuff\\assignments\\grad\\projects\\Julia\\PromptingTools.jl"))
 using PromptingTools
 const PT = PromptingTools
 const RT = PromptingTools.Experimental.RAGTools
@@ -26,6 +24,5 @@ export make_knowledge_packs, just_generate
 include("user_preferences.jl")
 include("utils.jl")
 export remove_urls_from_index
-
 
 end
