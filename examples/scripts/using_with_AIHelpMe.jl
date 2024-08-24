fe3db6703a83843de777b01c4aaafb797f6f9e7e
J S <49557684+svilupp@users.noreply.github.com>
2024-08-24 09:26:00 +0100

    update docs

--- examples/scripts/using_with_AIHelpMe.jl
+++ examples/scripts/using_with_AIHelpMe.jl
@@ -4,16 +4,22 @@ Pkg.add(url = "https://github.com/JuliaGenAI/DocsScraper.jl/")
 Pkg.add("AIHelpMe")
 using DocsScraper
 using AIHelpMe
-using AIHelpMe: pprint
+using AIHelpMe: pprint, last_result
 
 # Creating the index
 crawlable_urls = ["https://juliagenai.github.io/DocsScraper.jl/dev/home/"]
 index_path = make_knowledge_packs(crawlable_urls;
     index_name = "docsscraper", embedding_dimension = 1024, embedding_bool = true,
-    target_path = joinpath(pwd(), "knowledge_packs"))
+    target_path = "knowledge_packs")
 
-# Using the index with AIHelpMe
+# Using the index with AIHelpMe, load it as the default index
 AIHelpMe.load_index!(index_path)
 
-pprint(aihelp("what is DocsScraper.jl?"))
-pprint(aihelp("how do I install DocsScraper?"))
+# Ask questions // pprint is optional
+aihelp("what is DocsScraper.jl?") |> pprint
+
+aihelp("how do I install DocsScraper?") |> pprint
+
+# Get more detailed outputs with sources for the last answer
+# Identical to running aihelp(; return_all=true)
+last_result() |> pprint
